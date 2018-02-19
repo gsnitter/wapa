da5e8aa37593337cfaffa10738280ed1f789ddb3
Steffen Nitter <steffen_nitter@gmx.de>
2018-02-19 21:21:09 +0100

    Decent error message if no IMAGE_SOURCE-parameter is set in .env-file.

--- src/Lib/PictureSelector.php
+++ src/Lib/PictureSelector.php
@@ -11,7 +11,20 @@ class PictureSelector
 
     public function __construct()
     {
+        $this->checkImageSource();
         $this->pathes = glob(getenv('IMAGE_SOURCE') . '/*');
+    }
+
+    public function checkImageSource()
+    {
+        $dir = getenv('IMAGE_SOURCE');
+        if (!$dir) {
+            $error  = 'Parameter IMAGE_SOURCE (path to source images\' folder) not found in .env-file.';
+            throw new \Exception($error);
+        }
+        if (!file_exists(getenv('IMAGE_SOURCE'))) {
+            throw new \Exception("Please check parameter IMAGE_SOURCE. Folder {$dir} does not seem to exist.");
+        }
     }
 
     public function chooseOne(): string
